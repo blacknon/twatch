f7283df425f1df48cf3e6dbf8bd0064ec6737920
Blacknon <blacknon@orebibou.com>
2026-05-12 21:39:35 +0900

    update. ci test.

--- src/runner/pty.rs
+++ src/runner/pty.rs
@@ -1,3 +1,7 @@
+// Copyright (c) 2026 Blacknon. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
+
 use std::io::{Read, Write};
 use std::sync::atomic::AtomicU64;
 use std::sync::atomic::{AtomicBool, Ordering};
